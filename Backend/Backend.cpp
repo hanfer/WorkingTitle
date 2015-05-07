cee3766a91ccb6a7b6c5a1c1d8fdc9025fe239f1
Benedikt Kleiner <benedikt.kleiner@gmail.com>
2015-05-07 12:45:51 +0200

    pipe example

--- Backend/Backend.cpp
+++ Backend/Backend.cpp
@@ -1,25 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <uv.h>
 
-#include "Buffer.hpp"
+uv_loop_t *loop;
 
-int64_t counter = 0;
+void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
+	buf->base = (char *)malloc(suggested_size);
+	buf->len = suggested_size;
+}
 
-void wait_for_a_while(uv_idle_t* handle) {
-	counter++;
+void echo_write(uv_write_t *req, int status) {
+	if (status < 0) {
+		fprintf(stderr, "Write error %s\n", uv_err_name(status));
+	}
+	free(req);
+}
 
-	if (counter >= 10e6)
-		uv_idle_stop(handle);
+void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
+	if (nread < 0) {
+		if (nread != UV_EOF)
+			fprintf(stderr, "Read error %s\n", uv_err_name(nread));
+		uv_close((uv_handle_t*)client, NULL);
+		return;
+	}
+
+	uv_write_t *req = (uv_write_t *)malloc(sizeof(uv_write_t));
+	uv_buf_t wrbuf = uv_buf_init(buf->base, nread);
+	uv_write(req, client, &wrbuf, 1, echo_write);
+	free(buf->base);
+}
+
+void on_new_connection(uv_stream_t *server, int status) {
+	if (status == -1) {
+		// error!
+		return;
+	}
+
+	uv_pipe_t *client = (uv_pipe_t*)malloc(sizeof(uv_pipe_t));
+	uv_pipe_init(loop, client, 0);
+	if (uv_accept(server, (uv_stream_t*)client) == 0) {
+		uv_read_start((uv_stream_t*)client, alloc_buffer, echo_read);
+	}
+	else {
+		uv_close((uv_handle_t*)client, NULL);
+	}
+}
+
+void remove_sock(int sig) {
+	uv_fs_t req;
+	uv_fs_unlink(loop, &req, "echo.sock", NULL);
+	exit(0);
 }
 
 int main() {
-	uv_idle_t idler;
+	loop = uv_default_loop();
 
-	uv_idle_init(uv_default_loop(), &idler);
-	uv_idle_start(&idler, wait_for_a_while);
+	uv_pipe_t server;
+	uv_pipe_init(loop, &server, 0);
 
-	printf("Idling...\n");
-	uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+	signal(SIGINT, remove_sock);
 
-	uv_loop_close(uv_default_loop());
-	return 0;
+	int r;
+	if ((r = uv_pipe_bind(&server, "\\\\.\\pipe\\backend"))) {
+		fprintf(stderr, "Bind error %s\n", uv_strerror(r));
+		return 1;
+	}
+	if ((r = uv_listen((uv_stream_t*)&server, 128, on_new_connection))) {
+		fprintf(stderr, "Listen error %s\n", uv_strerror(r));
+		return 2;
+	}
+	return uv_run(loop, UV_RUN_DEFAULT);
 }
