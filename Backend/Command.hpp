c8f715292bba32108b4e1f90ad8d7fb71c5ac8be
Benedikt Kleiner <benedikt.kleiner@gmail.com>
2015-05-18 10:03:39 +0200

    some more commands

--- Backend/Command.hpp
+++ Backend/Command.hpp
@@ -1,10 +1,29 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+
+#include "Any.hpp"
+
 class Command
 {
 public:
+	Command() = delete;
+	Command(const AnyList& pParams)
+		: params(pParams)
+	{}
+
 	virtual ~Command() {}
 
 	virtual void execute() = 0;
 	virtual void undo() = 0;
+
+	const AnyList params;
+};
+
+class CreateBuffer : public Command
+{
+public:
+	void execute() override {};
+	void undo() override {};
 };
