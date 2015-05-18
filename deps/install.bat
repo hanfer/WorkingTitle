170e8a885b9b87616066be0a44776bb8e1e49db8
Benedikt Kleiner <benedikt.kleiner@gmail.com>
2015-05-18 10:00:57 +0200

    build libs in theright modi

--- deps/install.bat
+++ deps/install.bat
@@ -19,8 +19,9 @@ git clone --depth 1 --branch v2.7 https://github.com/akheron/jansson.git
 setlocal
 call "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"
 cd jansson
-cmake . -G "NMake Makefiles"
-nmake jansson
+cmake . -DJANSSON_BUILD_DOCS=0 -DJANSSON_STATIC_CRT=1 -DSTATIC_CRT=1 -G "Visual Studio 12 2013"
+msbuild jansson.sln /target:jansson /p:Configuration=Release
+msbuild jansson.sln /target:jansson /p:Configuration=Debug
 endlocal
 popd
 
@@ -36,6 +37,7 @@ git clone --depth 1 https://chromium.googlesource.com/external/gyp gyp
 set GYP_MSVS_VERSION=2013
 %python% gyp\gyp_main.py --depth=. http_parser.gyp
 call "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"
-msbuild http_parser.sln /target:http_parser
+msbuild http_parser.sln /target:http_parser /p:Configuration=Release
+msbuild http_parser.sln /target:http_parser /p:Configuration=Debug
 endlocal
 popd
